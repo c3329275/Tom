4b7d55ed8843f13a6069885cedbbdb439351d969
c3329275 <49225039+c3329275@users.noreply.github.com>
2019-04-18 22:08:32 +1000

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
+
+
 
 int main()
 {
@@ -9,37 +12,42 @@ int main()
     int B;
     int C;
     int D;//
-     
+
     printf("Type 'A' for ENCRYPTION using a rotation cipher \nType 'B' for DECRYPTION using a rotation cipher \nType 'C' for ENCRYPTION using a substitution cipher \nType 'D' for DECRYPTION using a substitution cipher \n");
     scanf("%c" , &choice);
-    
-    if(choice == 'A')
+
+if(choice == 'A')
     {
-       
-        char text[50], mte; //mte (Message to Encrypt) made of 124 bytes
-        int rn; //rotation number
-        
+
+
+    int rn; //rotation number
+    char text[124], mte; //mte (Message to Encrypt) made of 124 bytes
+
+
         printf("Type your message to encrypt here:\n"); //will print the prompt to enter the message to encrypt
         scanf("\n %[^\n]s" , text); // ^ used to scan or read a line of text from a standard input
         printf("Enter an encryption number:\n");
         scanf("%d", &rn);
-        for(rn = 0; text[rn] != '0' ; ++rn)
-        {
-            text = text[rn];
-                if(mte >= 'A' && mte <= 'Z')
-                {
-                    mte = mte + rn;
-                    if(text > 'Z')
-                    {
-                        mte = mte - 'Z' + 'A' -1;
-                    }
-                    text[rn] = mte;
-                }
+
+
+
+int i;
+for(i = 0; text[i] != '\0'; ++i){
+mte = text[i]; //text to encrypt (tte)
+
+    if (mte >= 'A' && mte <= 'Z')
+    {
+        mte = mte + rn;
+
+        if(mte > 'Z'){
+        mte = mte - 'Z' + 'A' -1;
         }
-        
-        printf("Your message is %s \n", text);
+    text[i] = mte;
+}
+}
+printf("The encrypted message is:\n%s" , text);
     }
-    
+
 
 
 return 0;
